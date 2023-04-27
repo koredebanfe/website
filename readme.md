a71c64a6a3f42c4883cb15db36fa4c2f50ae6256
koredebanfe <oluwakoredebanfe@gmail.com>
2023-04-26 18:52:27 -0700

    [200~

---
+++ readme.md
@@ -0,0 +1,22 @@
+<!-- question 2 -->
+
+Easy to learn
+Ability to combine HTML and JavaScript in one syntax as JSX
+component functionality
+Data flows unidirectional
+
+<!-- question 3 -->
+community support
+SEO friendliness
+Reusable Components
+
+<!-- question 4-->
+React was created by Jordan Walke but is been maintained by facebook
+
+<!-- question 5 -->
+All tags must be have the ending slash
+All event attributes must be written in camelCase in JSx
+All element must be wrapped in a single parent element
+
+<!-- question 6 -->
+JSX is not a valid JavaScript
---
+++ src/Main.jsx
@@ -0,0 +1,33 @@
+
+function Main() {
+    let country = 'Nigeria';
+  return (
+    <div>
+        <section>
+            <h2>
+                About Us 
+            </h2>
+            <p>
+              We are a small team of web developers based in the {country}.
+              We specialise in creating modern and responsive website for business and individuals.
+            </p>
+        </section>
+
+        <section>
+              <h2>
+                Our Services
+              </h2>
+
+              <ul>
+                <li>Web design</li>
+                <li>Web Development</li>
+                <li>E-commerce Solutions</li>
+                <li>Search Engine Optimisation (SEO)</li>
+              </ul>
+        </section>
+
+    </div>
+  )
+}
+
+export default Main
